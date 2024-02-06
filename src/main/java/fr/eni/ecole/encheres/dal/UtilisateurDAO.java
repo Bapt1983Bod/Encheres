40e6837ef018bfc849e326b19dff4e476802cf74
thoma <thoma@DESKTOP-49C9E2V>
2024-02-06 09:27:35 +0100

    dsdsqds

--- src/main/java/fr/eni/ecole/encheres/dal/UtilisateurDAO.java
+++ src/main/java/fr/eni/ecole/encheres/dal/UtilisateurDAO.java
@@ -1,5 +1,14 @@
 package fr.eni.ecole.encheres.dal;
 
+import java.util.Optional;
+
+import fr.eni.ecole.encheres.bo.Utilisateur;
+
 public interface UtilisateurDAO {
 
+	Optional<Utilisateur> findByPseudo(String pseudo);
+
+	Optional<Utilisateur> findByEmail(String email);
+
+	Utilisateur save(Utilisateur utilisateur);
 }
--- src/main/java/fr/eni/ecole/encheres/dal/UtilisateurDAOImpl.java
+++ src/main/java/fr/eni/ecole/encheres/dal/UtilisateurDAOImpl.java
@@ -1,5 +1,68 @@
 package fr.eni.ecole.encheres.dal;
 
-public class UtilisateurDAOImpl {
+import java.util.Optional;
 
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import fr.eni.ecole.encheres.bo.Utilisateur;
+
+@Repository
+public class UtilisateurDAOImpl implements UtilisateurDAO {
+
+	private static final String INSERT = "INSERT INTO UTILISATEURS (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) VALUES (:pseudo, :nom, :prenom, :email, :telephone, :rue, :codePostal, :ville, :motDePasse, :credit, :administrateur)";
+
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+	public UtilisateurDAOImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+		super();
+		this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+	}
+
+	public void creerUtilisateur(Utilisateur utilisateur) {
+		MapSqlParameterSource map = new MapSqlParameterSource();
+		map.addValue("pseudo", utilisateur.getPseudo());
+		map.addValue("nom", utilisateur.getNom());
+		map.addValue("prenom", utilisateur.getPrenom());
+		map.addValue("email", utilisateur.getEmail());
+		map.addValue("telephone", utilisateur.getTelephone());
+		map.addValue("rue", utilisateur.getRue());
+		map.addValue("codePostal", utilisateur.getCodePostal());
+		map.addValue("ville", utilisateur.getVille());
+		map.addValue("motDePasse", utilisateur.getMotDePasse());
+		map.addValue("credit", utilisateur.getCredit());
+		map.addValue("administrateur", utilisateur.getAdministrateur());
+
+		System.out.println(utilisateur);
+
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		this.namedParameterJdbcTemplate.update(INSERT, map, keyHolder);
+
+		if (keyHolder != null && keyHolder.getKey() != null) {
+
+			utilisateur.setNoUtilisateur(keyHolder.getKey().intValue());
+		}
+
+	}
+
+	@Override
+	public Optional<Utilisateur> findByPseudo(String pseudo) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<Utilisateur> findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Utilisateur save(Utilisateur utilisateur) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
