fb54a83844657be6c11944f6fe82667ba7b8b930
Bapt1983Bod <152866910+Bapt1983Bod@users.noreply.github.com>
2024-02-14 16:17:58 +0100

    maj

--- src/main/java/fr/eni/ecole/encheres/bll/EncheresServiceImpl.java
+++ src/main/java/fr/eni/ecole/encheres/bll/EncheresServiceImpl.java
@@ -7,52 +7,136 @@ import org.springframework.stereotype.Service;
 import fr.eni.ecole.encheres.bo.Enchere;
 import fr.eni.ecole.encheres.bo.Utilisateur;
 import fr.eni.ecole.encheres.dal.EncheresDAO;
+import fr.eni.ecole.encheres.dal.UtilisateurDAO;
 
 @Service
 public class EncheresServiceImpl implements EncheresService {
 
 	private EncheresDAO encheresDAO;
+	private UtilisateurDAO utilisateurDAO;
 
-	public EncheresServiceImpl(EncheresDAO encheresDAO) {
+	public EncheresServiceImpl(EncheresDAO encheresDAO, UtilisateurDAO utilisateurDAO) {
 		this.encheresDAO = encheresDAO;
+		this.utilisateurDAO = utilisateurDAO;
 	}
 
 	// Vérifier si vente ouverte avant d'insérer l'enchère (comparer date du jour
 	// avec date de début et date de fin de la vente
 	@Override
 	public void encherir(int noArticle, long montantEnchere, Utilisateur utilisateur) {
-		encheresDAO.encherir(noArticle, montantEnchere, utilisateur);
+		
+		if (utilisateur.getCredit() > montantEnchere) {
+			encheresDAO.createEnchere(utilisateur, noArticle, montantEnchere);
+			utilisateurDAO.updateCredit(utilisateur, montantEnchere);
+		} else {
+			// remonter erreur !!!
+		}
 	}
 
 	@Override
 	public void deleteByIdArticle(int noArticle) {
-		encheresDAO.deleteByIdArticle(noArticle);
+		
+		
+		// Récupération de la liste des enchères d'un article
+		List<Enchere> encheres = encheresDAO.findByArticle(noArticle);
+				
+		if (!encheres.isEmpty()) {
+			for (Enchere enchere : encheres) {
+				int montantEnchere = enchere.getMontantEnchere();
+				Utilisateur utilisateur = enchere.getUtilisateur();
+				// recrédit du montant des enchères avant suppression enchères
+				utilisateurDAO.restoreCredit(utilisateur, montantEnchere);
+			}
+		}
+		// Suppression des enchères
+		encheresDAO.deleteByArticle(noArticle);
 	}
 
 	@Override
 	public void deleteByNoUtilisateur(int noUtilisateur) {
-		encheresDAO.deleteByNoUtilisateur(noUtilisateur);
+		// Récupération de la liste des enchères d'un utilisateur
+		List<Enchere> encheres = encheresDAO.findByUtilisateur(noUtilisateur);
+		
+		if (!encheres.isEmpty()) {
+			for (Enchere enchere : encheres) {
+				// Remboursement du montant de l'enchère à l'utilisateur
+				int montantEnchere = enchere.getMontantEnchere();
+				Utilisateur utilisateur = enchere.getUtilisateur();
+				
+				utilisateurDAO.restoreCredit(utilisateur, montantEnchere);
+			}
+		}
+		// Suppression des enchères
+		encheresDAO.deleteByUtilisateur(noUtilisateur);
+		
+		
+		
+		
+	//	encheresDAO.deleteByNoUtilisateur(noUtilisateur);
 
 	}
 
 	@Override
 	public void deleteByIdUtilisateurAndIdArticle(int noArticle, Utilisateur utilisateur) {
-		encheresDAO.deleteByIdUtilisateurAndIdArticle(noArticle, utilisateur);
+		// Récupération du montant de l'enchère
+		int montantEnchere = encheresDAO.findEnchereUtilisateur(noArticle, utilisateur).getMontantEnchere();
+		
+		utilisateurDAO.restoreCredit(utilisateur, montantEnchere);
+		
+		encheresDAO.deleteByUtilisateurArticle(noArticle, utilisateur);
+		
+		
+		
+		
+	//	encheresDAO.deleteByIdUtilisateurAndIdArticle(noArticle, utilisateur);
 	}
 
 	@Override
 	public Enchere getHighestEnchere(int noArticle) {
-		return encheresDAO.getHighestEnchere(noArticle);
+		List<Enchere> encheres = encheresDAO.findByArticle(noArticle);
+		// numéro de l'article et montant ayant l'enchère la plus haute
+		Enchere enchere = new Enchere();
+		long montantEnchere = 0;
+		
+		for (Enchere e : encheres) {
+			if (e.getMontantEnchere()>montantEnchere) {
+				montantEnchere = e.getMontantEnchere();
+				enchere = e;
+			}
+		}
+		return enchere;
+		
+		
+		
+		
+	//	return encheresDAO.getHighestEnchere(noArticle);
 	}
 
 	@Override
 	public boolean aEncheriSurArticle(int noArticle, Utilisateur acheteur) {
-		return encheresDAO.aEncheriSurArticle(noArticle, acheteur);
+		if((encheresDAO.findEnchereUtilisateur(noArticle, acheteur))== null) {
+			return false;
+		} else {
+			return true;
+		}
+		
+		
+		
+		
+	//	return encheresDAO.aEncheriSurArticle(noArticle, acheteur);
 	}
 
 	@Override
 	public void surenchere(int noArticle, long montantEnchere, Utilisateur acheteur) {
-		encheresDAO.surenchere(noArticle, montantEnchere, acheteur);
+		if((encheresDAO.findEnchereUtilisateur(noArticle, acheteur)) != null) {
+			deleteByIdUtilisateurAndIdArticle(noArticle, acheteur);
+		}
+		
+		encheresDAO.createEnchere(acheteur, noArticle, montantEnchere);
+		
+		
+		
+	//	encheresDAO.surenchere(noArticle, montantEnchere, acheteur);
 
 	}
 
--- src/main/java/fr/eni/ecole/encheres/dal/EncheresDAO.java
+++ src/main/java/fr/eni/ecole/encheres/dal/EncheresDAO.java
@@ -7,19 +7,17 @@ import fr.eni.ecole.encheres.bo.Utilisateur;
 
 public interface EncheresDAO {
 
-	public void encherir(int noArticle, long montantEnchere, Utilisateur utilisateur);
-
-	public void deleteByIdUtilisateurAndIdArticle(int noArticle, Utilisateur utilisateur);
-
-	public void deleteByIdArticle(int noArticle);
-
-	public void deleteByNoUtilisateur(int noUtilisateur);
-
-	Enchere getHighestEnchere(int noArticle);
-
-	public void surenchere(int noArticle, long montantEnchere, Utilisateur acheteur);
-
-	boolean aEncheriSurArticle(int noArticle, Utilisateur utilisateur);
+//	public void deleteByIdUtilisateurAndIdArticle(int noArticle, Utilisateur utilisateur);
+//
+//	public void deleteByIdArticle(int noArticle);
+//
+//	public void deleteByNoUtilisateur(int noUtilisateur);
+//
+//	Enchere getHighestEnchere(int noArticle);
+//
+//	public void surenchere(int noArticle, long montantEnchere, Utilisateur acheteur);
+//
+//	boolean aEncheriSurArticle(int noArticle, Utilisateur utilisateur);
 	
 	// Renvoi la liste de toutes les enchères
 	public List<Enchere> findAll();
