4dd1bf3c6d54e57ef82eb55e7952161a98d732fa
Bapt1983Bod <152866910+Bapt1983Bod@users.noreply.github.com>
2024-02-15 15:47:40 +0100

    rien

--- src/main/java/fr/eni/ecole/encheres/configuration/security/SecurityConfig.java
+++ src/main/java/fr/eni/ecole/encheres/configuration/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -78,7 +80,8 @@ public class SecurityConfig {
 				.permitAll() // détermine qui a accès à la deconnection
 		);
 
-		// Parametrage de l'expiratio de session
+		
+		
 
 		return http.build();
 	}
